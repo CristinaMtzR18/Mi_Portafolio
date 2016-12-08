2ff64d281a34d3c6fe367e32c7ea90d852344958
CristinaMtzR18 <cris.mtz.r18@gmail.com>
2016-12-08 13:16:41 -0600

    Mi Portafolio

--- js/skills.js
+++ js/skills.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-	//Barra de progreso
+  //Barra de progreso INTERMEDIO
   var bar = new ProgressBar.Line(progresoIntermedio, {
     strokeWidth: 6,
-  	easing: 'easeInOut',
-  	duration: 25000,
-  	color: '#26A69A',
-  	trailColor: '#A8A8A8',
-  	trailWidth: 6,
-  	svgStyle: {width: '80%', height: '100%'}
-	});
+    easing: 'easeInOut',
+    duration: 25000,
+    color: '#26A69A',
+    trailColor: '#A8A8A8',
+    trailWidth: 6,
+    svgStyle: {width: '80%', height: '100%'}
+  });
   bar.setText('Intermedio');
-	bar.animate(.50);
+  bar.animate(.50);
   bar.text.style.color = '#fffaf0';
   bar.text.style.fontSize = '1rem';
   bar.text.style.fontWeight ='bold';
   
-  //Barra de progreso
+  //Barra de progreso BASICO
   var bar = new ProgressBar.Line(progresoBasico, {
     strokeWidth: 6,
     easing: 'easeInOut',
@@ -30,5 +30,11 @@ $(document).ready(function(){
   bar.text.style.color = '#fffaf0';
   bar.text.style.fontSize = '1rem';
   bar.text.style.fontWeight ='bold';
-
+	//Grafica de corazon
+  var bar = new ProgressBar.Path('#heart-path', {
+  easing: 'easeInOut',
+  duration: 35000
+  });
+  bar.set(0);
+  bar.animate(1.0);
 });
